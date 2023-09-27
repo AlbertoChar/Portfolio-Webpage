1dcec9a6a52b0a3da0e60cc85c66a4d9912a9279
Alberto Charabati <bcharabati@icloud.com>
2023-09-27 20:09:12 +0300

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Skills } from './components/Skills';
 import { Projects } from './components/Projects';
 import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
+import { Analytics } from '@vercel/analytics/react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
