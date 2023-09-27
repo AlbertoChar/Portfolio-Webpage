000655eab9bb8052f208a4307fdd431fd2ceac16
Alberto Charabati <bcharabati@icloud.com>
2023-09-27 20:09:53 +0300

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -12,12 +12,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-        <NavBar />
-        <Banner />
-        <Skills />
-        <Projects />
-        <Contact />
-        <Footer />
+      <Analytics />
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 }
